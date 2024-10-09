20a2f4e05f6c120b7853c2a16f02df1c1e51fd65
Rustam Royeen <rustam.royeen@gmail.com>
2024-10-09 18:21:35 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/ticketingprojectmvc_me/TicketingProjectMvcMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectmvc_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectMvcMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
