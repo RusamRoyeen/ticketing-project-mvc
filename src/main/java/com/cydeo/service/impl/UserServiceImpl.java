c519aee6dbda68a93de0ce5a32361c64ee113c3a
Rustam Royeen <rustam.royeen@gmail.com>
2024-10-15 19:28:04 -0400

    Part 3, Update function is completed.

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -29,4 +29,9 @@ public class UserServiceImpl extends AbstractMapService<UserDTO, String> impleme
     public void deleteById(String username) {
         super.deleteById(username);
     }
+
+    @Override
+    public void update(UserDTO object) {
+        super.update(object.getUserName(), object);
+    }
 }
